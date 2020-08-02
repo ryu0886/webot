f84ab4bf42ca6962964281f682dc84cbe3bd53d4
Ryan Yu <ryu@paloaltonetworks.com>
2020-08-01 17:24:04 -0700

    update

--- webot/views.py
+++ webot/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from webot import app
 
@@ -11,23 +11,63 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ["zip"]
 
 @app.route('/')
-@app.route('/page1', methods=["GET", "POST"])
-def page1():
+def root():
+    return redirect(url_for("page1"))
+
+@app.route('/upload', methods=["GET","POST"])
+def upload():
+    #ref01 = request.args.get('ref01', default = 0, type = int)
+    #if ref01 == 0: return redirect(url_for("page1"))
+    print("upload>>")
+    b_redir = False
     if request.method == "POST":
+        print(request.files)
         if "file" in request.files:
             file = request.files["file"]
             if file.filename != "":
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
-                    print(file.read())
-                    return render_template(
-                        'result.html',
-                        title='Result',
-                        year=datetime.now().year,
-                    )
+                    #print(file.read())
+                    print(filename)
+                    b_redir = True
+
+
+    print("upload<<")
+    if b_redir:
+        print("redirect...")
+        #Redirect on Ajax calls doesn't work, browser doesn't handle it.
+        #return redirect(url_for("result"), code=302)
+        return jsonify({"redirect": "/result"})
+    else:
+        return redirect(url_for("page1"))
+
+@app.route('/page1', methods=["GET"])
+def page1():
+    return redirect(url_for("uploadfile3"))
+
+@app.route('/uploadfile0', methods=["GET"])
+def uploadfile0():
     """Renders the home page."""
     return render_template(
-        'page1.html',
+        'uploadfile0.html',
+        title='Analyzer',
+        year=datetime.now().year,
+    )
+
+@app.route('/uploadfile3', methods=["GET"])
+def uploadfile3():
+    """Renders the home page."""
+    return render_template(
+        'uploadfile3.html',
+        title='Analyzer',
+        year=datetime.now().year,
+    )
+
+@app.route('/uploadfile4', methods=["GET"])
+def uploadfile4():
+    """Renders the home page."""
+    return render_template(
+        'uploadfile4.html',
         title='Analyzer',
         year=datetime.now().year,
     )
